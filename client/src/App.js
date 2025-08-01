29231eaf8eea570b1c37fbf956cb2b98a3490531
Yash <102345366+yash1876@users.noreply.github.com>
2025-08-02 00:26:16 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import About from "./pages/About";
 import Calendar from "./components/Calendar";
 import Appointments from "./pages/Appointments";
 import Admin from "./pages/Admin";
